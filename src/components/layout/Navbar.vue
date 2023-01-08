f8fc7ba2e6c2b3e8a2962a5bea6857cdc8f1c85c
Andrey Baglay <andreybaglai@gmail.com>
2023-01-08 12:34:16 +0200

    add project

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-3 border-b"></div>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Apple Color Emoji",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+      montserrat: ["Montserrat", "sans-serif"],
+      code: ["Source Code Pro", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
